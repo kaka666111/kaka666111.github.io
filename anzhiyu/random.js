63197139bbdb0aefd92201bcf07a5245b5e1ac47
kaka666111 <3134677405@qq.com>
2025-07-16 20:46:34 +0800

    Site updated: 2025-07-16 20:46:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/05/DDL是最大的生产力/","2025/07/16/ctf练习录/","2024/12/13/hello-world/","2024/11/17/这是用来测试的哦/"];function toRandomPost(){
+var posts=["2024/12/05/DDL是最大的生产力/","2024/12/13/hello-world/","2024/11/17/这是用来测试的哦/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
